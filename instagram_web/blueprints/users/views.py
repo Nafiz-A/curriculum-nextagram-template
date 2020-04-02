af31a8f5bab298492be8837398b5c224fc98946a
n-Ashraf <nafizashraf6@gmail.com>
2020-04-02 19:17:05 +0800

    working on csrf---hashed donne

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
-
-
+from flask import Blueprint, render_template, request
+from werkzeug.security import generate_password_hash
+from flask_wtf.csrf import CSRFProtect
+# csrf = CSRFProtect()
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
@@ -9,6 +10,14 @@ users_blueprint = Blueprint('users',
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
+
+
+@users_blueprint.route("/sign_up", methods=['POST'])
+def sign_up():
+    username = request.form.get('username')
+    password = request.form.get('pass')
+    hashed = generate_password_hash(password)
+    return render_template('users/sign_up.html', username=request.form.get('username'), hashed=generate_password_hash(password))
 
 
 @users_blueprint.route('/', methods=['POST'])
