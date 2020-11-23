e768024c20b95c1f50d559b900684f0833f1d595
n-Ashraf <nafizashraf6@gmail.com>
2020-11-23 13:22:57 +0800

    pyment removed

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -3,7 +3,6 @@ from flask import render_template
 from instagram_web.blueprints.users.views import users_blueprint
 from instagram_web.blueprints.sessions.views import sessions_blueprint
 from instagram_web.blueprints.images.views import images_blueprint
-from instagram_web.blueprints.payment.views import payment_blueprint
 from instagram_web.blueprints.following.views import following_blueprint
 from flask_assets import Environment, Bundle
 from .util.assets import bundles
@@ -15,7 +14,6 @@ from flask_wtf.csrf import CSRFProtect
 app.register_blueprint(users_blueprint, url_prefix="/users")
 app.register_blueprint(sessions_blueprint, url_prefix="/sessions")
 app.register_blueprint(images_blueprint, url_prefix="/images")
-app.register_blueprint(payment_blueprint, url_prefix="/payment")
 app.register_blueprint(following_blueprint, url_prefix="/following")
 
 
